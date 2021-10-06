84eee2321545be81e47a04af783d19ed7cef0129
Rodol21 <rodol21reina@gmail.com>
2021-10-05 21:11:36 -0400

    test

---
+++ app/src/main/java/edu/cse570/helloworld/MainActivity.java
@@ -0,0 +1,32 @@
+package edu.cse570.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initDisplayButton();
+    }
+
+    private void initDisplayButton() {
+        Button displayButton = findViewById(R.id.displayButton);
+        displayButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                EditText editName = findViewById(R.id.editTextName);
+                TextView textDisplay = findViewById(R.id.textViewDisplay);
+                String nameToDisplay = editName.getText().toString();
+                textDisplay.setText("Hello " + nameToDisplay);
+            }
+        });
+    }
+}
